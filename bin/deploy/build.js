c8048456e62d12564fae6313abdbd7fa0d0d4ee9
Han <xsytby1112@gmail.com>
2019-06-24 14:11:43 +0800

    docs: add comment for deploy script

--- bin/deploy/build.js
+++ bin/deploy/build.js
@@ -14,17 +14,29 @@ const templateList = glob.sync('*', {
   ignore: '*.zip'
 })
 
-function buildIndex(data = [], src) {
+/**
+ *create the index.html for templates
+ *
+ * @param {Array} [data=[]] templateList array
+ * @param {String} target target path
+ */
+function buildIndex(data = [], target) {
   let html = fse.readFileSync(path.join(__dirname, './index.html'), { encoding: 'utf-8' })
   const complied = _.template(html)
   html = complied({ data })
-  fse.writeFileSync(src, html)
+  fse.writeFileSync(target, html)
 }
 
+/**
+ *create a child_process with promise
+ *
+ * @param {String} name process name for log
+ * @param {*} args spawn arguments
+ * @returns {Promise}
+ */
 function createPs(name, ...args) {
   return new Promise((resolve, reject) => {
     const ps = spawn(...args)
-    // ps.stdout.on('data', data => console.log(data.toString('utf-8')))
     ps.on('error', console.trace)
     ps.on('close', (code) => {
       if (code !== 0) {
@@ -36,7 +48,16 @@ function createPs(name, ...args) {
   })
 }
 
+/**
+ * @class BuildProcess
+ */
 class BuildProcess {
+  /**
+   *Creates an instance of BuildProcess, which use to run nuxt build
+   * @param {String} templateName framework tempalte name, used to a folder name
+   * @param {Object} [options={}] spawn options
+   * @memberof BuildProcess
+   */
   constructor(templateName, options = {}) {
     const { env = {}, ...opts } = options
     this.folderName = templateName
@@ -52,18 +73,37 @@ class BuildProcess {
     }
   }
 
+  /**
+   *install dependents
+   *
+   * @param {Object} options
+   * @returns {Promise} spawn options
+   * @memberof BuildProcess
+   */
   installDeps(options) {
     const action = `${this.folderName} install`
     console.log(action + 'ing...')
     return createPs(action, 'yarn', ['install'], { ...this.options, ...options })
   }
 
+  /**
+   *build nuxt project
+   *
+   * @param {Object} options
+   * @returns {Promise}
+   * @memberof BuildProcess
+   */
   build(options) {
     const action = `${this.folderName} build`
     console.log(action + 'ing...')
     return createPs(action, 'yarn', ['build'], { ...this.options, ...options })
   }
 
+  /**
+   *move some file, because the assets path is not correct after build success
+   *
+   * @memberof BuildProcess
+   */
   moveToDist() {
     const targetDir = deployDir(this.folderName)
     const sourceDir = path.join(this.options.cwd, 'dist')
@@ -79,6 +119,12 @@ class BuildProcess {
     fse.moveSync(sourceDir, targetDir, { overwrite: true })
   }
 
+  /**
+   *the total process of build a nuxt project
+   *
+   * @returns {Promise}
+   * @memberof BuildProcess
+   */
   run() {
     return this.installDeps()
       .then(() => this.build())
@@ -91,19 +137,6 @@ class BuildProcess {
 }
 
 // Serial build
-// const buildList = templateList.map(item => new BuildProcess(item, env[item].run))
-
-// Promise.all(templateList.map(item => new BuildProcess(item, { env: env[item] }).run()))
-//   .then((data) => {
-//     buildIndex(data, deployDir('index.html'))
-//     console.log('finish')
-//   })
-//   .catch((e) => {
-//     console.trace(e)
-//     process.exit(1)
-//   })
-
-// parallel build
 const templateData = []
 
 const afterBuild = templateList.reduce((p, item) => {
@@ -115,12 +148,9 @@ const afterBuild = templateList.reduce((p, item) => {
 
 afterBuild
   .then(() => buildIndex(templateData, deployDir('index.html')))
-  .catch((e) => {
-    if (e) {
-      console.trace(e)
+  .catch((err) => {
+    if (err) {
+      console.trace(err)
       process.exit(1)
     }
   })
-
-// const item = 'nuxt-element-dashboard'
-// new BuildProcess(item, { env: env[item] }).run()
