34d347ac26c358d2c693eda5127854b76f6d2809
Han <xsytby1112@gmail.com>
2019-06-27 16:54:53 +0800

    feat(framework): filter formatDate add default format parameter
    Closes #2

--- template/frameworks/jest/test/formatDate.test.js
+++ template/frameworks/jest/test/formatDate.test.js
@@ -1,7 +1,21 @@
 import {formatDate} from '@/const/filter.js'
 
+const date = 0
+
 describe('formatDate', () => {
   test('no params', () => {
-    expect(formatDate()).toBeFalsy()
+    expect(formatDate()).toBe('')
+  })
+
+  test('default formatter: YYYY-MM-DD', () => {
+    expect(formatDate(date)).toBe('1970-01-01')
+  })
+
+  test('formatter: YYYY/MM/DD', () => {
+    expect(formatDate(date, 'YYYY/MM/DD')).toBe('1970/01/01')
+  })
+
+  test('formatter: YYYY,MM,DD', () => {
+    expect(formatDate(date, 'YYYY,MM,DD')).toBe('1970,01,01')
   })
 })
