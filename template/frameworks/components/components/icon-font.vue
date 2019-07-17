3f77833489251ba1215dec024b86eee2709c2c75
ouzuyu <oupenguin@gmail.com>
2019-07-17 14:21:01 +0800

    refactor: 修改 icon-font 组件

--- template/frameworks/components/components/icon-font.vue
+++ template/frameworks/components/components/icon-font.vue
@@ -1,18 +1,29 @@
 <template>
-  <i :class="`${iconClass} ${icon}`"></i>
+  <i :class="iconClass"></i>
 </template>
 
 <script>
+// 若引入了其他字体库，可根据该字体库 class 前缀拓展此对象
+const ICON_PREFIX = {
+  el: 'el-icon-',
+  fa: 'fa fa-',
+  iconfont: 'iconfont icon-'
+}
+
 export default {
   name: 'IconFont',
   props: {
     icon: {
       type: String
+    },
+    prefix: {
+      type: String,
+      default: 'el'
     }
   },
   computed: {
     iconClass() {
-      return this.icon.startsWith('fa') ? 'fa' : 'iconfont'
+      return `${ICON_PREFIX[this.prefix]}${this.icon}`
     }
   }
 }
