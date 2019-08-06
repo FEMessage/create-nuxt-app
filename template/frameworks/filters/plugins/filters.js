1797bec046f170e74e6468b603e02dd129f3242c
Donald Shen <825870831@qq.com>
2019-08-06 17:45:49 +0800

    refactor: 优化price实现

--- template/frameworks/filters/plugins/filters.js
+++ template/frameworks/filters/plugins/filters.js
@@ -10,17 +10,11 @@ function isNull(o) {
 // 123456 => 123,456
 export function price(val, cancelFixed) {
   if (isNull(val) || isNaN(val)) return
-  let USPrice = Number.parseFloat(val).toLocaleString()
-
-  let lastDot = USPrice.toString().indexOf('.')
-  // 完全是整数, 需要添加小数点
-  if (lastDot === -1 && !cancelFixed) USPrice += '.00'
-
-  // 返回数据是一位小数，用0补齐为两位小数
-  if (USPrice.toString().substring(lastDot + 1).length === 1 && !cancelFixed)
-    USPrice += '0'
-
-  return '¥' + USPrice
+  return parseFloat(val).toLocaleString('zh-Hans-CN', {
+    style: 'currency',
+    currency: 'CNY',
+    minimumFractionDigits: cancelFixed ? 0 : 2
+  })
 }
 
 // 时间戳格式化
