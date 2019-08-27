1993304a7eaf5d9fb35a17c435405381861f36cb
evillt <hujiehao@deepexi.com>
2019-08-27 15:21:12 +0800

    chore: use `console.error`

--- template/frameworks/axios/plugins/axios.js
+++ template/frameworks/axios/plugins/axios.js
@@ -68,7 +68,7 @@ export default function({$axios, store, app, redirect}) {
       }
     } else {
       // TODO asyncData 的错误 需要日志监控
-      console.log('error', error)
+      console.error('error', error)
     }
 
     // 将错误信息继续抛出，业务逻辑可以进行后续的操作
