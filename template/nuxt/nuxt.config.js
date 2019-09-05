86b3ed9b17cf5d304f6914075bd70460eb8981d3
evillt <hujiehao@deepexi.com>
2019-09-05 13:45:12 +0800

    feat: use pwa module

--- template/nuxt/nuxt.config.js
+++ template/nuxt/nuxt.config.js
@@ -166,7 +166,9 @@ module.exports = {
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     // Doc: https://github.com/nuxt-community/dotenv-module
-    ['@nuxtjs/dotenv', {path: './'}]
+    ['@nuxtjs/dotenv', {path: './'}],
+    // Doc: https://pwa.nuxtjs.org/
+    '@nuxtjs/pwa'
   ],
   axios
 }
