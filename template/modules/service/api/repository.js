d85273288c77261dd8aea14d5b9ab5631513993f
Han <xsytby1112@gmail.com>
2020-01-16 14:19:10 +0800

    refactor: 优化 CRUD 接口类

--- template/modules/service/api/repository.js
+++ template/modules/service/api/repository.js
@@ -1,9 +1,41 @@
 /*
- * @Author: Han 
- * @Date: 2020-01-13 17:22:51 
+ * @Author: Han
+ * @Date: 2020-01-13 17:22:51
  * @Last Modified by: Han
- * @Last Modified time: 2020-01-14 11:41:41
+ * @Last Modified time: 2020-01-16 14:20:22
  */
+
+/**
+ *可以通过继承 Repository 来实现不满足 RESTful 最佳实践的接口
+ *这是一个例子, 接口的 query 上必须带 appId，接口的设计包含多个主从 id 等
+ * @export
+ * @class ExampleRepository
+ * @extends {Repository}
+ */
+export class ExampleRepository extends Repository {
+  constructor(resource, id) {
+    super(resource)
+    this.id = id
+  }
+
+  uri(appId) {
+    return `${this.resource}/status/${this.id}?appId=${appId}`
+  }
+
+  update($axios) {
+    return (appId, payload, ...args) =>
+      $axios.$post(`${this.uri(appId)}`, payload, ...args)
+  }
+}
+
+const RepositoryInterface = {
+  URI: 'uri',
+  CREATE: 'create',
+  LIST: 'list',
+  DETAIL: 'detail',
+  UPDATE: 'update',
+  DELETE: 'delete',
+}
 
 /**
  * 作为 service 层 API 的基类
@@ -28,7 +60,7 @@ export class Repository {
    * @returns {String} API Uri
    * @memberof Repository
    */
-  uri() {
+  [RepositoryInterface.URI]() {
     return this.resource
   }
 
@@ -40,7 +72,7 @@ export class Repository {
    * @returns function
    * @memberof Repository
    */
-  create($axios) {
+  [RepositoryInterface.CREATE]($axios) {
     return (payload, ...args) => $axios.$post(this.uri(), payload, ...args)
   }
 
@@ -52,7 +84,7 @@ export class Repository {
    * @returns function
    * @memberof Repository
    */
-  show($axios) {
+  [RepositoryInterface.DETAIL]($axios) {
     return (id, ...args) => $axios.$get(`${this.uri()}/${id}`, ...args)
   }
 
@@ -64,7 +96,7 @@ export class Repository {
    * @returns function
    * @memberof Repository
    */
-  index($axios) {
+  [RepositoryInterface.LIST]($axios) {
     return (...args) => $axios.$get(this.uri(), ...args)
   }
 
@@ -76,8 +108,9 @@ export class Repository {
    * @returns function
    * @memberof Repository
    */
-  update($axios) {
-    return (id, payload, ...args) => $axios.$put(`${this.uri()}/${id}`, payload, ...args)
+  [RepositoryInterface.UPDATE]($axios) {
+    return (id, payload, ...args) =>
+      $axios.$put(`${this.uri()}/${id}`, payload, ...args)
   }
 
   /**
@@ -88,48 +121,37 @@ export class Repository {
    * @returns {Function}
    * @memberof Repository
    */
-  delete($axios) {
+  [RepositoryInterface.DELETE]($axios) {
     return (id, ...args) => $axios.$delete(`${this.uri()}/${id}`, ...args)
   }
 
   /**
    *Init such a interface for developer
+   * 不建议外部覆盖此方法，为了保证调用者，可以使用统一的 API 调用方法。
    * @private
    * @param {@nuxtjs/axios} $axios
    * @returns {Object}
    * @memberof Repository
    */
   init($axios) {
+    // bind this 是为了继承 Repository 的属性方法，方便扩展
     return {
-      // uri 用于 el-data-table 等组件
-      uri: this.uri.bind(this),
-      create: this.create($axios),
-      show: this.show($axios),
-      index: this.index($axios),
-      update: this.update($axios),
-      delete: this.delete($axios)
+      [RepositoryInterface.URI]: this[RepositoryInterface.URI].bind(this),
+      [RepositoryInterface.CREATE]: this[RepositoryInterface.CREATE](
+        $axios,
+      ).bind(this),
+      [RepositoryInterface.DETAIL]: this[RepositoryInterface.DETAIL](
+        $axios,
+      ).bind(this),
+      [RepositoryInterface.LIST]: this[RepositoryInterface.LIST]($axios).bind(
+        this,
+      ),
+      [RepositoryInterface.DELETE]: this[RepositoryInterface.DELETE](
+        $axios,
+      ).bind(this),
+      [RepositoryInterface.UPDATE]: this[RepositoryInterface.UPDATE](
+        $axios,
+      ).bind(this),
     }
-  }
-}
-
-/**
- *可以通过继承 Repository 来实现不满足 RESTful 最佳实践的接口
- *这是一个例子, 接口的 query 上必须带 appId，接口的设计包含多个主从 id 等
- * @export
- * @class ExampleRepository
- * @extends {Repository}
- */
-export class ExampleRepository extends Repository {
-  constructor(resource, id) {
-    super(resource)
-    this.id = id
-  }
-
-  uri(appId) {
-    return `${this.resource}/status/${this.id}?appId=${appId}`
-  }
-
-  update($axios) {
-    return (appId, payload, ...args) => $axios.$post(`${this.uri(appId)}`, payload, ...args)
   }
 }
