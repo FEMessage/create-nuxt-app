bc7d3893e3a5871d40375071e75f0501a4c8c350
snowlocked <git@detective.uu.me>
2020-09-16 17:21:35 +0800

    fix: -d默认true

--- bin/cli.js
+++ bin/cli.js
@@ -10,7 +10,7 @@ cli.help()
 cli.option('-l, --list', 'the list of preset template') // 优先级 1
 cli.option('-a, --all', 'generate all preset template') // 优先级 2
 cli.option('-t, --template <template>', 'create a preset template')
-cli.option('-d, --docker <Y(yes)/n(no)>', 'use docker build')
+cli.option('-d, --docker', 'use docker build')
 cli.option('-o, --output <output>', 'the output path of the generator')
 
 function run(config, outDir) {
@@ -30,30 +30,17 @@ function run(config, outDir) {
 
 ;(function main({options, args: [folder]}) {
   const outDir = options.o || '.'
-  const dockerMap = {
-    true: true,
-    Y: true,
-    yes: true,
-    false: false,
-    n: false,
-    no: false,
-  }
   if (options.l) {
     configs.forEach(item => console.log(item.template))
   } else if (options.a) {
-    configs.forEach(c =>
-      run(
-        {...c, docker: dockerMap[options.d || options.docker] || false},
-        outDir,
-      ),
-    )
+    configs.forEach(c => run({...c, docker: 'd' in options}, outDir))
   } else {
     const config = {
       ...(folder ? {folder} : {}),
       ...(options.t ? {template: options.t} : {}),
     }
     if ('d' in options) {
-      config.docker = dockerMap[options.d || options.docker]
+      config.docker = true
     }
     run(config, outDir)
   }
